aed88ab95324c8b5bf7ad9922b601e5f34d5a411
Ivan Stoliarov <ivan.s@absoluteweb.com>
2026-06-23 13:22:44 +0200

    supabase starts

---
+++ app/account/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'Account',
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+        Welcome, Ivan
+      </h2>
+    </div>
+  );
+}
