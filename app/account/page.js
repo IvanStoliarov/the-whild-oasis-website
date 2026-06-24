1b15c92e887e02dd66ee9147f77e68531c0e2dbf
Ivan Stoliarov <ivan.s@absoluteweb.com>
2026-06-24 22:21:48 +0200

    feat(auth): add Google authentication flow

--- app/account/page.js
+++ app/account/page.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { auth } from '../_lib/auth';
 
 export const metadata = {
   title: 'Account',
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(' ').at(0);
   return (
     <div>
       <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
-        Welcome, Ivan
+        Welcome, {firstName}
       </h2>
     </div>
   );
