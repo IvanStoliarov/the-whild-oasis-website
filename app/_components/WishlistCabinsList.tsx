756417011fd89a918f0856bf164ce91e7bb9ff9b
Ivan Stoliarov <ivan.s@absoluteweb.com>
2026-07-15 17:58:22 +0200

    refactor(wishlist): simplify cabin card rendering

--- app/_components/WishlistCabinsList.tsx
+++ app/_components/WishlistCabinsList.tsx
@@ -12,19 +12,13 @@ export default async function WishlistCabinsList() {
   if (!session?.user.guestId) return null;
   const userWishlist = await getWishlistItems(session?.user.guestId);
   const userWishlistIds = userWishlist.map(el => el.cabinId);
-  const cabinsInWishlist = new Set(userWishlistIds);
   const cabinsToShow = await getCabins(userWishlistIds);
   if (cabinsToShow.length < 1)
     return <p>You have no cabins in your wishlist</p>;
   return (
     <div className='grid sm:grid-cols-1 gap-8 lg:gap-12 xl:gap-14'>
       {cabinsToShow.map(cabin => (
-        <CabinCard
-          cabinsInWishlist={cabinsInWishlist}
-          isLoggedIn={true}
-          cabin={cabin}
-          key={cabin.id}
-        />
+        <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
   );
